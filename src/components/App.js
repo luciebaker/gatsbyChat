744b80469fdd34eea27fa033cbc060933a367c40
luciebaker <luciebdev@gmail.com>
2020-01-27 07:50:06 -0500

    side pannel and channels

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { Grid } from 'semantic-ui-react';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 
+const App = ({ currentUser }) => (
+  <Grid columns="equal" className="app" style={{ background: '#eee'}}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser}/>
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        
-      <h1>App</h1>
-      </div>
-    );
-  }
-}
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
 
-export default App;
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  
+  </Grid>
+)
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(App);
