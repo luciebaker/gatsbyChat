57318fe56fdb0db762a82e8ecec49b1ccf600f23
luciebaker <luciebdev@gmail.com>
2020-01-28 08:12:12 -0500

    direct messages

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,20 +3,24 @@ import { Header, Segment, Icon, Input } from 'semantic-ui-react';
 
 class MessagesHeader extends React.Component {
     render() {
+        const { channelName, numUniqueUsers, handleSearchChange, searchLoading, isPrivateChannel } = this.props;
+
         return (
             <Segment clearing>
                 {/* Channel Title */}
                 <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }} >
                     <span> 
-                    Channel
-                    <Icon name={"star outline"} color="black" />
+                    {channelName}
+                    {!isPrivateChannel && <Icon name={"star outline"} color="black" />}
                     </span>
-                    <Header.Subheader> 2 users </Header.Subheader>
+                    <Header.Subheader>{numUniqueUsers}</Header.Subheader>
                 </Header>
 
                  {/* Channel Serach Input */}
                 <Header floated="right">
                     <Input 
+                        loading={searchLoading}
+                        onChange={handleSearchChange}
                         size="mini"
                         icon="search"
                         name="searchTerm"
