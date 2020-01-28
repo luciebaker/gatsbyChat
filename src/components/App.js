981535573547ab8863b0cea4090ba868ca152c48
luciebaker <luciebdev@gmail.com>
2020-01-28 12:13:35 -0500

    metapanel

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { Grid } from 'semantic-ui-react';
-import ColorPanel from './ColorPanel/ColorPanel';
-import SidePanel from './SidePanel/SidePanel';
-import Messages from './Messages/Messages';
-import MetaPanel from './MetaPanel/MetaPanel';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./App.css";
+import { connect } from "react-redux";
 
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
-  <Grid columns="equal" className="app" style={{ background: '#eee'}}>
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel 
-      key={currentUser && currentUser.uid}
-      currentUser={currentUser}
-    />
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages 
+      <Messages
         key={currentChannel && currentChannel.id}
         currentChannel={currentChannel}
         currentUser={currentUser}
@@ -25,16 +23,21 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
     </Grid.Column>
 
     <Grid.Column width={4}>
-      <MetaPanel />
+      <MetaPanel
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        isPrivateChannel={isPrivateChannel}
+        userPosts={userPosts}
+      />
     </Grid.Column>
-  
   </Grid>
-)
+);
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel: state.channel.isPrivateChannel
-})
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts
+});
 
 export default connect(mapStateToProps)(App);
